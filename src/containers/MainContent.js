36a4a4d6f8c5392dab70c11a80c0256dedff0bfa
haleighdalke <haleighdalke@gmail.com>
2020-08-19 12:27:06 -0400

    resolved all but css from scott haleigh merge

--- src/containers/MainContent.js
+++ src/containers/MainContent.js
@@ -1,37 +1,16 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import Button from 'react-bootstrap/Button'
-import AccomplishmentsPopUp from './AccomplishmentsPopUp';
 import ChartContainer from './ChartContainer';
 import NavbarContainer from './NavbarContainer'; 
 import {Container} from 'reactstrap'
 
 class MainContent extends React.Component {
-    state = {
-        modalShow: false
-    }
-
-    setModalShow = () => {
-        this.setState({
-            modalShow: !this.state.modalShow
-        })
-    }
 
     render(){
     let {user, token, goals, habits} = this.props
     return(
-        <div>
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
-            <h1>Location: {user.location}</h1>
-            <button onClick={() => {
-                localStorage.clear()
-                this.props.history.push('/')
-                }}>Log Out</button>
-            <Button variant="primary" onClick={this.setModalShow}>Add an Accomplishment</Button>
-            <AccomplishmentsPopUp show={this.state.modalShow} onHide={this.setModalShow} goals={goals} habits={habits}/>
         <Container className="main-content">
-            <NavbarContainer user={user} token={token} goals={goals}/>
+            <NavbarContainer user={user} token={token} goals={goals} habits={habits}/>
             <ChartContainer/>
         </Container>
     )}
--- src/containers/NavbarContainer.js
+++ src/containers/NavbarContainer.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Container, Button } from 'reactstrap';
 import logo from '../assets/img/fit-task-ic-logo6.png'
+import AccomplishmentsPopUp from './AccomplishmentsPopUp'
 
 class NavbarContainer extends React.Component {
+    state = {
+        modalShow: false
+    }
+
+    setModalShow = () => {
+        this.setState({
+            modalShow: !this.state.modalShow
+        })
+    }
   
     render(){
-        let {user, token, goals} = this.props
+        let {user, token, goals, habits} = this.props
         return(
             <Container className="sidenav-container">
                 <div className="logo">
@@ -20,6 +30,9 @@ class NavbarContainer extends React.Component {
                     <a href="#" >Habit Handler</a>
                     <a href="#" >Goals Handler</a>
                 </div>
+                <br></br>
+                <Button variant="primary" onClick={this.setModalShow}>Add an Accomplishment</Button>
+                <AccomplishmentsPopUp show={this.state.modalShow} onHide={this.setModalShow} goals={goals} habits={habits}/>
                 <br></br>
                 <Button onClick={() => {
                     localStorage.clear()
